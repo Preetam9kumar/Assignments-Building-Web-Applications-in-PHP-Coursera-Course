e520016ca88ec57d929fd3eb94751da10a6071b2
Preetam Kumar <103277784+Preetam9kumar@users.noreply.github.com>
2024-02-26 11:52:09 +0500

    Update guess.php
    changed method POST to GET method

--- guess.php
+++ guess.php
@@ -37,19 +37,19 @@ margin-top:50px;
 <?php
 $ans=0;
 $msg='';
-if($_SERVER['REQUEST_METHOD']=='POST' && $_POST['submit']){
-    if(isset($_POST['guess']) && !empty($_POST['guess']) && is_numeric($_POST['guess'])){
-        $ans = $_POST['guess'];
+if($_SERVER['REQUEST_METHOD']=='GET' && $_GET['submit']){
+    if(isset($_GET['guess']) && !empty($_GET['guess']) && is_numeric($_GET['guess'])){
+        $ans = $_GET['guess'];
     }
-    if (empty($_POST['guess'])){ 
+    if (empty($_GET['guess'])){ 
         $msg = "Missing guess parameter";
-      } else if ($_POST['guess'] < 1 ) {
+      } else if ($_GET['guess'] < 1 ) {
         $msg = "Your guess is too short";
-      } else if ( ! is_numeric($_POST['guess']) ) {
+      } else if ( ! is_numeric($_GET['guess']) ) {
         $msg = "Your guess is not a number";
-      } else if ( $_POST['guess'] < 42 ) {
+      } else if ( $_GET['guess'] < 42 ) {
         $msg = "Your guess is too low";
-      } else if ( $_POST['guess'] > 42 ) {
+      } else if ( $_GET['guess'] > 42 ) {
         $msg = "Your guess is too high";
       } else {
         $msg = "Congratulations - You are right";
@@ -60,7 +60,7 @@ if($_SERVER['REQUEST_METHOD']=='POST' && $_POST['submit']){
 
 <div class="calc">
 <h1 style="padding-left:30px">Welcome to my guessing game</h1>
-<form action="<?=$_SERVER['PHP_SELF'] ?>" method="POST">
+<form action="<?=$_SERVER['PHP_SELF'] ?>" method="GET">
  <div class="calc-display">
 <h1> You Guessed : <?= number_format($ans,2) ?></h1>
 <h1><?= $msg ?></h1>
